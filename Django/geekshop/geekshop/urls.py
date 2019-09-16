c712955ce97bbd7ee3ba599c296e62b2ca5d1e3c
pythonwizard <amicuswat@gmail.com>
2019-09-16 18:39:36 +0400

    Все сломалось. Комчу чтобы заменить на возможно рабочий код и посмотреть почему сломалось.

--- Django/geekshop/geekshop/urls.py
+++ Django/geekshop/geekshop/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('auth/', include('authapp.urls', namespace='auth')),
     path('basket/', include('basketapp.urls', namespace='basket')),
     path('contact/', mainapp.contact, name='contact'),
-    path('admin/', admin.site.urls),
+    path('admin/', include('adminapp.urls', namespace='admin')),
 ]
 
 if settings.DEBUG:
