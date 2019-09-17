14152226c832756e1a4d2dd36e6c1dbfe5bfcd43
pythonwizard <amicuswat@gmail.com>
2019-09-17 17:17:09 +0400

    Стабильная версия

--- Django/geekshop/adminapp/views.py
+++ Django/geekshop/adminapp/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render, get_object_or_404
 from authapp.models import ShopUser
+from django.shortcuts import get_object_or_404, render, HttpResponseRedirect
+from django.contrib.auth.decorators import user_passes_test
 from mainapp.models import Product, ProductCategory
+from django.urls import reverse
+from authapp.forms import ShopUserRegisterForm
+from adminapp.forms import ShopUserAdminEditForm, ProductCategoryEditForm, ProductEditForm
 
-# Create your views here.
+# Эквивалент лямбды
+# def test(user):
+#     return user.is_superuser
 
+@user_passes_test(lambda u: u.is_superuser)
 def users(request):
     title = 'админка/пользователи'
 
-    users_list = ShopUser.objects.all().order_by('-is_active',
-                                            '-is_superuser',
-                                            '-is_staff',
-                                            'username')
+    users_list = ShopUser.objects.all().order_by('-is_active', '-is_superuser', '-is_staff', 'username')
 
     content = {
         'title': title,
@@ -19,15 +23,61 @@ def users(request):
 
     return render(request, 'adminapp/users.html', content)
 
+@user_passes_test(lambda u: u.is_superuser)
 def user_create(request):
-    pass
+    title = 'пользователи/создание'
 
+    if request.method == 'POST':
+        user_form = ShopUserRegisterForm(request.POST, request.FILES)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse('admin:users'))
+
+    else:
+        user_form = ShopUserRegisterForm()
+
+    content = {'title': title, 'update_form': user_form}
+
+    return render(request, 'adminapp/user_update.html', content)
+
+
+
+@user_passes_test(lambda u: u.is_superuser)
 def user_update(request, pk):
-    pass
+    title = 'пользователи/редактирование'
 
+    edit_user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == 'POST':
+        edit_form = ShopUserAdminEditForm(request.Post, request.FILES, instance=edit_user)
+
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('admin:user_update', args=[edit_user.pk]))
+
+    else:
+        edit_form = ShopUserAdminEditForm(instance=edit_user)
+
+    content = {'title': title, 'update_form': edit_form}
+
+    return render(request, 'adminapp/user_update.html', content)
+
+
+@user_passes_test(lambda u: u.is_superuser)
 def user_delete(request, pk):
-    pass
+    title = 'ползователи/удаление'
 
+    user = get_object_or_404(ShopUser, pk=pk)
+
+    if request.method == 'POST':
+        user.is_active = False
+        user.save()
+        return HttpResponseRedirect(reverse('admin:users'))
+
+    content = {'title': title, 'user_to_delete': user}
+
+    return render(request, 'adminapp/user_delete.html', content)
+
+@user_passes_test(lambda u: u.is_superuser)
 def categories(request):
     title = 'админка/категории'
 
@@ -40,37 +90,128 @@ def categories(request):
 
     return render(request, 'adminapp/categories.html', content)
 
+@user_passes_test(lambda u: u.is_superuser)
 def category_create(request):
-    pass
+    title = 'категории/создание'
 
+    if request.method == 'POST':
+        category_form = ProductCategoryEditForm(request.POST, request.FILES)
+        if category_form.is_valid():
+            category_form.save()
+            return HttpResponseRedirect(reverse('admin:categories'))
+
+    else:
+        category_form = ProductCategoryEditForm()
+
+    content = {'title': title, 'update_form': category_form}
+
+    return render(request, 'adminapp/category_update.html', content)
+
+@user_passes_test(lambda u: u.is_superuser)
 def category_update(request, pk):
-    pass
+    title = 'пользователи/редактирование'
 
+    edit_category = get_object_or_404(ProductCategory, pk=pk)
+    if request.method == 'POST':
+        edit_form = ShopUserAdminEditForm(request.Post, request.FILES, instance=edit_category)
+
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('admin:categoty_update', args=[edit_category.pk]))
+
+    else:
+        edit_form = ProductCategoryEditForm(instance=edit_category)
+
+    content = {'title': title, 'update_form': edit_form}
+
+    return render(request, 'adminapp/category_update.html', content)
+
+@user_passes_test(lambda u: u.is_superuser)
 def category_delete(request, pk):
     pass
 
+@user_passes_test(lambda u: u.is_superuser)
 def products(request, pk):
-    title = 'админка/продукты'
+    title = 'админка/продукт'
 
     category = get_object_or_404(ProductCategory, pk=pk)
-    products_list = Product.objects.filter(category_pk=pk).order_by('name')
+    products_list = Product.objects.filter(category__pk=pk).order_by('name')
 
     content = {
         'title': title,
         'category': category,
-        'objects': products_list
+        'objects': products_list,
     }
 
     return render(request, 'adminapp/products.html', content)
 
+@user_passes_test(lambda u: u.is_superuser)
 def product_create(request, pk):
-    pass
+    title = 'продукт/создание'
+    category = get_object_or_404(ProductCategory, pk=pk)
 
+    if request.method == 'POST':
+        product_form = ProductEditForm(request.POST, request.FILES)
+        if product_form.is_valid:
+            product_form.save()
+            return HttpResponseRedirect(reverse('admin:products', args=[pk]))
+
+    else:
+        product_form = ProductEditForm(initial={'category': category})
+
+    content = {
+               'title': title,
+               'update_form': product_form,
+               'category': category
+               }
+
+    return render(request, 'adminapp/product_update.html', content)
+
+
+@user_passes_test(lambda u: u.is_superuser)
 def product_read(request, pk):
-    pass
+    title = 'продукт/подробнее'
+    product = get_object_or_404(Product, pk=pk)
+    content = {
+        'title': title,
+        'object': product
+    }
 
+    return render(request, 'adminapp/product_read.html', content)
+
+@user_passes_test(lambda u: u.is_superuser)
 def product_update(request, pk):
-    pass
+    title = 'продукты/редактирвать'
 
+    edit_product = get_object_or_404(Product, pk)
+
+    if request.method == 'POST':
+        edit_form = ProductEditForm(request.POST, request.FILES, instance=edit_product)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('admin:product_update', args=edit_product.pk))
+
+    else:
+        edit_form = ProductEditForm(instance=edit_product)
+        content = {
+            'title': title,
+            'update_form': edit_form,
+            'category': edit_product.category
+        }
+
+        return render(request, 'adminapp/product_update.html', content)
+
+@user_passes_test(lambda u: u.is_superuser)
 def product_delete(request, pk):
-    pass
+    title = 'продукт/удаление'
+
+    product = get_object_or_404(Product, pk=pk)
+
+    if request.method == 'POST':
+        product.is_active = False
+        product.save()
+        return HttpResponseRedirect(reverse('admin:products', args=[product.category.pk]))
+
+    content = {'title': title, 'product_to_delete': product}
+
+    return render(request, 'adminapp/product_delete.html', content)
--- Django/geekshop/geekshop/urls.py
+++ Django/geekshop/geekshop/urls.py
@@ -1,23 +1,22 @@
 
-from django.contrib import admin
+import mainapp.views as mainapp
 from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import path
-
-import mainapp.views as mainapp
 
 urlpatterns = [
     path('', mainapp.main, name='main'),
     path('products/', include('mainapp.urls', namespace='products')),
+    path('contact/', mainapp.contact, name='contact'),
     path('auth/', include('authapp.urls', namespace='auth')),
     path('basket/', include('basketapp.urls', namespace='basket')),
-    path('contact/', mainapp.contact, name='contact'),
+
     path('admin/', include('adminapp.urls', namespace='admin')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
